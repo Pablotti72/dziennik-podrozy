140c89dff03c54d38412886baa0082f384cf587c
Pablotti72 <pawwasi@gmail.com>
2026-08-27 15:52:56 +0100

    Create firebase-config.js

---
+++ firebase-config.js
@@ -0,0 +1,9 @@
+export const firebaseConfig = {
+    apiKey: "TWÓJ_API_KEY",
+    authDomain: "TWÓJ_PROJECT_ID.firebaseapp.com",
+    databaseURL: "https://TWÓJ_PROJECT_ID-default-rtdb.firebaseio.com",
+    projectId: "TWÓJ_PROJECT_ID",
+    storageBucket: "TWÓJ_PROJECT_ID.appspot.com",
+    messagingSenderId: "TWÓJ_SENDER_ID",
+    appId: "TWÓJ_APP_ID"
+};
