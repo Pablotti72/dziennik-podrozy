6ca2eea56345e44861c4d75f00b4a76e20f54a2c
Pablotti72 <pawwasi@gmail.com>
2026-08-27 16:45:24 +0100

    Delete firebase-config.js

--- firebase-config.js
+++
@@ -1,9 +0,0 @@
-export const firebaseConfig = {
-  apiKey: "AIzaSyBcRB_dec2OunBPevgemcWouCfDP6dNZV4",
-  authDomain: "moj-dziennik-podrozy.firebaseapp.com",
-  databaseURL: "https://moj-dziennik-podrozy-default-rtdb.europe-west1.firebasedatabase.app",
-  projectId: "moj-dziennik-podrozy",
-  storageBucket: "moj-dziennik-podrozy.firebasestorage.app",
-  messagingSenderId: "997572019753",
-  appId: "1:997572019753:web:0f5aa78a1c24072de3be38"
-};
